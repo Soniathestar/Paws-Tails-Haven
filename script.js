793343402b7a077e491e8a1336eaa1f1a4112b05
Sonia Ngaruiya <soniangaruiya@gmail.com>
2026-02-17 12:19:52 +0300

    Make form functionable

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+alert("JS is working");
 const heading = document.getElementById("Headerone");
 const text = heading.textContent;
 heading.textContent = "";
@@ -10,7 +11,27 @@ for(let char of text) {
     } else {
         span.textContent = char;
     }
-    span.style.display = "inline-block";
-    heading.appendChild(span);
+    span.style.display = "inline-block"
+
+
+    function showService(type) {
+  const detailsText = document.getElementById('detailstext');
+  const detailsBox = document.getElementById('servicedetails');
+
+  const info = {
+    grooming: "At Paws&Tails, our grooming includes a full bubble bath, nail trimming, and a stylish haircut to make your pet the talk of the neighborhood.",
+    boarding: "Our boarding haven provides a safe, comforable and caring environment where your dog feels right at home. With regular feeding,daily playtime and constant supervision, we ensure that your furry friend is relaxed, happy and well cared for while you are away",
+    training: "Our training programs focus on positive reinforcment to help dogs develop good behavior, confidence and obedience.Each session is tailored to your dog's personality and learning pace, strengthening the bond between you and your compainion",
+    adoption:"We are dedicated to giving dogs a second chance through responsible adoption and rehoming.By careful matching of dogs with loving families, we help create lasting bonds and happy forever homes",
+    photography:"Our dog photography sessions capture your dog's unique pesonality in a calm and friendly setting. From playful moments to gentle expressions, we create timeless memories you'll treasure forever!"
+  };
+
+  if (info[type]){
+    detailsText.innerText = info[type];
+  detailsBox.style.animation = 'none';
+  detailsBox.offsetHeight; 
+  detailsBox.style.animation = null;
+  }else {
+  console.error("Service type not found"+ type);
+} 
 }
- 
